ea6825cfba8bf0d1fa1f4ac0ec8093cf995bff71
saumiyaanbu <itssaumiya@gmail.com>
2024-03-11 16:58:12 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "batch-22" 
+    default     =  "dev-server" 
 }
 
 variable "instance_type" { 
